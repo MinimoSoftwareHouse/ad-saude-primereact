f02abc01e76516bba07ef9fcd0251386487c6897
mxxm0x <aronr.chaves@gmail.com>
2023-05-31 23:58:02 -0300

    criando componente Footer com folha de estilos

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,38 @@
+import './Footer.css';
+import Logo from '../../assets/companyLogo.png';
+
+function Footer() {
+	return (
+		<>
+			<footer className="footer">
+				<div className="column">
+					<img
+						src={Logo}
+						alt="Company Logo"
+					/>
+				</div>
+				<div className="column2">
+					<h3>Menu</h3>
+					<ul>
+						<li>Home</li>
+						<li>Quem Somos</li>
+						<li>Serviços</li>
+						<li>Treinamentos</li>
+						<li>Contato</li>
+						<li>E-Social</li>
+						<li>Blog</li>
+					</ul>
+				</div>
+				<div className="column2">
+					<h3>Entre em Contato</h3>
+					<ul>
+						<li>WHATSAPP: (91) 98601-8137</li>
+						<li>E-mail: CONTATO@GSTOCUPACIONAL.COM.BR</li>
+					</ul>
+				</div>
+			</footer>
+		</>
+	);
+}
+
+export default Footer;
