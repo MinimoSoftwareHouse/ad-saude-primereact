6333a4af058ed09aa58945b131903a1e1e056a44
mxxm0x <aronr.chaves@gmail.com>
2023-05-31 23:58:44 -0300

    adicionando componente Treinamentos e folhas de estilos

---
+++ src/components/Treinamentos/Treinamentos.jsx
@@ -0,0 +1,45 @@
+import './Treinamentos.css';
+
+const Serviços = () => {
+	return (
+		<div className="containerServiços">
+			<div className="list">
+				<div className="services1">
+					<h1 className="title">Treinamentos de Segurança</h1>
+					<ul className="list">
+						<li className="item">
+							Treinamento de integração voltado para atividade de cada empresa
+						</li>
+
+						<li className="item">Treinamento para CIPA</li>
+						<li className="item">Treinamento de EPI’S</li>
+						<li className="item">Treinamento de Máquinas e Equipamentos</li>
+						<li className="item">Treinamento de Ergonomia no Trabalho</li>
+						<li className="item">Treinamento para Construção Civil</li>
+						<li className="item">
+							Treinamento para uso de Combustíveis e Inflamáveis
+						</li>
+						<li className="item">Treinamento de benzeno</li>
+						<li className="item">
+							Treinamento em segurança do trabalho e meio ambiente
+						</li>
+						<li className="item">Treinamento de Combate ao Incêndio</li>
+						<li className="item">Treinamento de Trabalho em Altura</li>
+						<li className="item">
+							Palestras educativas volta para Segurança em SIPAT
+						</li>
+					</ul>
+					<a
+						target="_blank"
+						rel="noreferrer"
+						href="https://wa.me/5591986018137?text=Olá! Gostaria de fazer um orçamento"
+						className="button-64">
+						Solicite um orçamento
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Serviços;
