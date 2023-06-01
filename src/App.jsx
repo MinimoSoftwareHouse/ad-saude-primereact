cacc16c7854c458ca478babf590cce84f71a6fb0
mxxm0x <aronr.chaves@gmail.com>
2023-05-31 23:57:08 -0300

    adicionando componentes no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import MapComponent from './components/MapComponent';
+import Serviços from './components/Serviços/Serviços.jsx';
+import Treinamentos from './components/Treinamentos/Treinamentos.jsx';
+import QuemSomos from './components/QuemSomos/QuemSomos.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import './App.css';
+
 function App() {
 	return (
 		<>
-			<MapComponent />
+			<div className="app-container">
+				<Treinamentos />
+				<Treinamentos />
+				<Treinamentos />
+			</div>
+			<Footer />
 		</>
 	);
 }
