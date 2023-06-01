2ec9f0ca7172ad0b2ff8ea1c353d3ebe19f80295
mxxm0x <aronr.chaves@gmail.com>
2023-06-01 00:10:23 -0300

    testando posicionamento no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import Serviços from './components/Serviços/Serviços.jsx';
 import Treinamentos from './components/Treinamentos/Treinamentos.jsx';
 import QuemSomos from './components/QuemSomos/QuemSomos.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import Main from './components/Main/Main.jsx';
 import './App.css';
 
 function App() {
 	return (
 		<>
 			<div className="app-container">
-				<Treinamentos />
-				<Treinamentos />
-				<Treinamentos />
+				<Main />
 			</div>
 			<Footer />
 		</>
