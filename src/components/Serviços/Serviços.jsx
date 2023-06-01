1dd41a0b3c83978d1c4a1813b7d0dcf824fb5481
mxxm0x <aronr.chaves@gmail.com>
2023-05-31 23:58:36 -0300

    adicionando componente Serviços e folhas de estilos

---
+++ src/components/Serviços/Serviços.jsx
@@ -0,0 +1,85 @@
+import './Serviços.css';
+
+const Serviços = () => {
+	return (
+		<div className="containerServiços">
+			<div className="list">
+				<div className="services1">
+					<h1 className="title">Nossos serviços</h1>
+					<ul className="list">
+						<li className="item">PGR - Programa de Gerenciamento de Riscos</li>
+						<li className="item">
+							PPP - Perfil Profissiográfico Previdenciário
+						</li>
+						<li className="item">Investigação de Acidente do Trabalho</li>
+						<li className="item">OSS - Ordem de Serviços de Segurança</li>
+						<li className="item">Prontuário de NR 20</li>
+						<li className="item">
+							Elaboração de LTCAT- Laudo Técnico de Condições Ambientais do
+							Trabalho
+						</li>
+						<li className="item">Avaliações Ambientais</li>
+						<li className="item">Treinamentos</li>
+						<li className="item">Informe do e-Social - Eventos de SST</li>
+						<li className="item">Acompanhamento de Perícias</li>
+					</ul>
+				</div>
+
+				<div className="services2">
+					<h1 className="title">Medicina Ocupacional</h1>
+					<ul className="list">
+						<li className="item">ASO – Atestado de Saúde Ocupacional</li>
+						<li className="item">Admissional</li>
+						<li className="item">Periódico</li>
+						<li className="item">Demissional</li>
+						<li className="item">Mudança de Função</li>
+						<li className="item">Retorno ao Trabalho</li>
+						<li className="item">
+							PCMSO (Programa de Controle Médico e Saúde Ocupacional)
+						</li>
+						<li className="item">CAT (Comunicado de Acidente de Trabalho)</li>
+						<li className="item">Exames Laboratoriais</li>
+						<li className="item">PCA (Programa de Conservação Auditivo)</li>
+						<li className="item">Atendimento Clínico do Funcionário</li>
+					</ul>
+				</div>
+
+				<div className="services3">
+					<h1 className="title">Prevenção e Combate a Incêndio</h1>
+					<ul className="list">
+						<li className="item">
+							Liberação de Auto de Vistoria do Corpo de Bombeiros (AVCB) –
+							Comercial e Industrial
+						</li>
+
+						<li className="item">
+							Liberação de Auto de Vistoria do Corpo de Bombeiros (AVCB) –
+							Eventos Temporários
+						</li>
+
+						<li className="item">
+							Renovação de Auto de Vistoria do Corpo de Bombeiros (AVCB)
+						</li>
+						<li className="item">Composição de Brigada de Incêndio</li>
+						<li className="item">Mudança de Função</li>
+						<li className="item">Retorno ao Trabalho</li>
+						<li className="item">
+							PCMSO (Programa de Controle Médico e Saúde Ocupacional)
+						</li>
+						<li className="item">CAT (Comunicado de Acidente de Trabalho)</li>
+						<li className="item">Exames Laboratoriais</li>
+						<li className="item">PCA (Programa de Conservação Auditivo)</li>
+						<li className="item">
+							<h4>
+								Elaboramos todos os tipos de laudos
+								(elétricos/mecânicos/estruturais) para Liberação de AVCB
+							</h4>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Serviços;
