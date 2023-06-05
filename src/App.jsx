15d9ab41357e52939dc2693f53cead273567ec62
mxxm0x <aronr.chaves@gmail.com>
2023-06-05 10:47:42 -0300

    usando Footer.jsx dentro do Router em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 
-import Footer from './components/Footer/Footer.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import QuemSomos from './components/QuemSomos/QuemSomos.jsx';
 
 function App() {
 	return (
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route exact path="/quem-somos" element={ <QuemSomos /> }> </Route>
-				{/* Carregar outros componentes aqui */}
-			</Routes>
-			<Footer />
-		</Router>
+		<>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route
+						exact
+						path="/quem-somos"
+						element={<QuemSomos />}>
+						{' '}
+					</Route>
+					{/* Carregar outros componentes aqui */}
+				</Routes>
+				<Footer />
+			</Router>
+		</>
 	);
 }
 
