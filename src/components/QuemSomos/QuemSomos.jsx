993aa007735a46b692003e545779eeb666924738
mxxm0x <aronr.chaves@gmail.com>
2023-06-05 11:03:18 -0300

    limpando quem somos

--- src/components/QuemSomos/QuemSomos.jsx
+++ src/components/QuemSomos/QuemSomos.jsx
@@ -3,73 +3,70 @@ import './QuemSomos.css';
 const QuemSomos = () => {
 	return (
 		<>
-			<div className="containerQuemSomos">
-				<div className="grid">
-					<div className="card">
-						<div className="card-content">
-							<h1>Missão</h1>
-							<h3>
-								Oferecer serviços e produtos de Saúde, Segurança e Higiene
-								Ocupacional, que promovam melhores condições de trabalho, bem
-								estar social, qualidade de vida e aumento da produtividade nas
-								empresas, através da adequação do ambiente de trabalho às normas
-								de saúde ocupacional.
-							</h3>
-						</div>
-					</div>
-
-					<div className="card">
-						<div className="card-content">
-							<h1>Visão</h1>
-							<h3>
-								Trabalhamos para ser uma empresa diferenciada na prestação de
-								serviços de forma a garantir a nossos clientes, serviços de
-								qualidade com atendimento personalizado visando à necessidade de
-								cada um.
-							</h3>
-						</div>
-					</div>
-
-					<div className="card">
-						<div className="card-content">
-							<h1>Valores</h1>
-							<h3>
-								Trabalho, Ética, Honestidade, Integridade, Inovação, Dinamismo,
-								Foco no cliente e Excelência no atendimento.
-							</h3>
-						</div>
+			<div className="grid">
+				<div className="card">
+					<div className="card-content">
+						<h1>Missão</h1>
+						<h3>
+							Oferecer serviços e produtos de Saúde, Segurança e Higiene
+							Ocupacional, que promovam melhores condições de trabalho, bem
+							estar social, qualidade de vida e aumento da produtividade nas
+							empresas, através da adequação do ambiente de trabalho às normas
+							de saúde ocupacional.
+						</h3>
 					</div>
 				</div>
 
-				<div className="textContent">
-					<p className="quemsomos-text">
-						Nossa História A GST ocupacional, é uma empresa especializada em
-						segurança e medicina do trabalho, iniciou suas atividades em janeiro
-						de 2019, estando sediada à Av. Mario Lazani, nº 565, Jardim América
-						– Andradas, Minas Gerais – MG, sede própria. Sua equipe técnica é
-						formada por Técnicos de Segurança do Trabalho, Engenheiros de
-						Segurança do Trabalho e Médicos do Trabalho. Dispõe de todos os
-						recursos técnicos (equipamentos de avaliação) necessários para o
-						pleno desempenho das atividades a que se destina. <br />
-					</p>
-					<p className="quemsomos-text">
-						Suas instalações e equipamentos permitem uma elaboração dos serviços
-						com qualidade e eficiência, além da agilidade para a entrega dos
-						serviços de medicina e segurança do trabalho. <br />
-					</p>
-
-					<p className="quemsomos-text">
-						Os profissionais vinculados a GST têm como preocupação o seu
-						aprimoramento técnico e legal, buscando sempre o crescimento pessoal
-						e profissional, através de cursos de atualização e especialização
-						constantes. Atuamos também como Peritos e Assistentes Técnicos. A
-						experiência de sua equipe vem ganhando credibilidade junto a um
-						grande número de empresas. A GST tem como objetivo básico a
-						preservação e a integridade das empresas, promovendo a saúde física
-						e mental do trabalhador, bem como a melhoria das condições e
-						ambiente do trabalho.
-					</p>
+				<div className="card">
+					<div className="card-content">
+						<h1>Visão</h1>
+						<h3>
+							Trabalhamos para ser uma empresa diferenciada na prestação de
+							serviços de forma a garantir a nossos clientes, serviços de
+							qualidade com atendimento personalizado visando à necessidade de
+							cada um.
+						</h3>
+					</div>
 				</div>
+
+				<div className="card">
+					<div className="card-content">
+						<h1>Valores</h1>
+						<h3>
+							Trabalho, Ética, Honestidade, Integridade, Inovação, Dinamismo,
+							Foco no cliente e Excelência no atendimento.
+						</h3>
+					</div>
+				</div>
+			</div>
+
+			<div className="textContent">
+				<p className="quemsomos-text">
+					Nossa História A GST ocupacional, é uma empresa especializada em
+					segurança e medicina do trabalho, iniciou suas atividades em janeiro
+					de 2019, estando sediada à Av. Mario Lazani, nº 565, Jardim América –
+					Andradas, Minas Gerais – MG, sede própria. Sua equipe técnica é
+					formada por Técnicos de Segurança do Trabalho, Engenheiros de
+					Segurança do Trabalho e Médicos do Trabalho. Dispõe de todos os
+					recursos técnicos (equipamentos de avaliação) necessários para o pleno
+					desempenho das atividades a que se destina. <br />
+				</p>
+				<p className="quemsomos-text">
+					Suas instalações e equipamentos permitem uma elaboração dos serviços
+					com qualidade e eficiência, além da agilidade para a entrega dos
+					serviços de medicina e segurança do trabalho. <br />
+				</p>
+
+				<p className="quemsomos-text">
+					Os profissionais vinculados a GST têm como preocupação o seu
+					aprimoramento técnico e legal, buscando sempre o crescimento pessoal e
+					profissional, através de cursos de atualização e especialização
+					constantes. Atuamos também como Peritos e Assistentes Técnicos. A
+					experiência de sua equipe vem ganhando credibilidade junto a um grande
+					número de empresas. A GST tem como objetivo básico a preservação e a
+					integridade das empresas, promovendo a saúde física e mental do
+					trabalhador, bem como a melhoria das condições e ambiente do trabalho.
+				</p>
 			</div>
 		</>
 	);
