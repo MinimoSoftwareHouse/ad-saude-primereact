f097014da3879e6df79ab7041433c6f74595e4b7
mxxm0x <aronr.chaves@gmail.com>
2023-06-05 12:57:04 -0300

    desenvolvendo tela home inicial

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,24 @@
 import './Home.css';
 import heroImg from '../../assets/heroImg.jpg';
+import QuemSomos from '../../components/QuemSomos/QuemSomos.jsx';
+import Serviços from '../../components/Serviços/Serviços.jsx';
+
 function Home() {
 	return (
-		<div>
-			<source srcset="" />
-		</div>
+		<>
+			<div className="hero-container">
+				<div className="text-container">
+					<h1>O que fazemos?</h1>
+					<h2>Segurança do Trabalho e Medicina Ocupacional</h2>
+				</div>
+				<img
+					className="hero-img"
+					src={heroImg}
+					alt="test"
+				/>
+			</div>
+			<QuemSomos />
+		</>
 	);
 }
 
