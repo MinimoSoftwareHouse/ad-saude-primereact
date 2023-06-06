eddaee234518c7377a9f3fec3fe9d28749dab696
Carlos Gabriel <carlosufpa10@gmail.com>
2023-06-06 12:32:19 -0300

    resolve conflitos

--- src/components/Serviços/Serviços.jsx
+++ src/components/Serviços/Serviços.jsx
@@ -32,60 +32,6 @@ const Serviços = () => {
 						))}
 					</ul>
 				</div>
-
-				<div className="services2">
-					<h1 className="title">Medicina Ocupacional</h1>
-					<ul className="list">
-						<li className="item">ASO – Atestado de Saúde Ocupacional</li>
-						<li className="item">Admissional</li>
-						<li className="item">Periódico</li>
-						<li className="item">Demissional</li>
-						<li className="item">Mudança de Função</li>
-						<li className="item">Retorno ao Trabalho</li>
-						<li className="item">
-							PCMSO (Programa de Controle Médico e Saúde Ocupacional)
-						</li>
-						<li className="item">CAT (Comunicado de Acidente de Trabalho)</li>
-						<li className="item">Exames Laboratoriais</li>
-						<li className="item">PCA (Programa de Conservação Auditivo)</li>
-						<li className="item">Atendimento Clínico do Funcionário</li>
-					</ul>
-				</div>
-
-				<div className="services3">
-					<h1 className="title">Prevenção e Combate a Incêndio</h1>
-					<ul className="list">
-						<li className="item">
-							Liberação de Auto de Vistoria do Corpo de Bombeiros (AVCB) –
-							Comercial e Industrial
-						</li>
-
-						<li className="item">
-							Liberação de Auto de Vistoria do Corpo de Bombeiros (AVCB) –
-							Eventos Temporários
-						</li>
-
-						<li className="item">
-							Renovação de Auto de Vistoria do Corpo de Bombeiros (AVCB)
-						</li>
-						<li className="item">Composição de Brigada de Incêndio</li>
-						<li className="item">Mudança de Função</li>
-						<li className="item">Retorno ao Trabalho</li>
-						<li className="item">
-							PCMSO (Programa de Controle Médico e Saúde Ocupacional)
-						</li>
-						<li className="item">CAT (Comunicado de Acidente de Trabalho)</li>
-						<li className="item">Exames Laboratoriais</li>
-						<li className="item">PCA (Programa de Conservação Auditivo)</li>
-						<li className="item">
-							<h4>
-								Elaboramos todos os tipos de laudos
-								(elétricos/mecânicos/estruturais) para Liberação de AVCB
-							</h4>
-						</li>
-					</ul>
-					
-				</div>
 			</div>
 		</div>
 	);
