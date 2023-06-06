d9364ae1d7075561fba24561c063145ee6bb12cd
Carlos Gabriel <carlosufpa10@gmail.com>
2023-06-06 17:54:50 -0300

    altera menu do footer para localização

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,27 +5,21 @@ import { Divider } from 'primereact/divider';
 function Footer() {
 	return (
 		<div className="page-container">
-			<footer className="footer w-full grid align-content-center absolute">
+			<footer className="footer w-full grid flex justify-content-center align-items-center absolute">
 				<Divider
-					align="center"
-					className="footer-divider"
+					align="bottom"
+					className="footer-divider mx-auto pt-6"
 				/>
-				<div className="md:col-4 flex justify-content-start align-items-center">
+				<div className="md:col-2 flex mx-auto justify-content-start">
 					<img
 						src={Logo}
 						alt="Company Logo"
 						className="footer-logo"
 					/>
 				</div>
-				<div className="items md:col-2">
-					<h3>Menu</h3>
-					<ul>
-						<li>Home</li>
-						<li>Quem Somos</li>
-						<li>Serviços</li>
-						<li>Treinamentos</li>
-						<li>Contato</li>
-					</ul>
+				<div className="items md:col-4 mr-5">
+					<h3>Nossa localização</h3>
+					<p>BR-316, Posto yamaga Próx. Unama Br - salas g8 e g9</p>
 				</div>
 				<div className="items md:col-4">
 					<h3>Entre em Contato</h3>
