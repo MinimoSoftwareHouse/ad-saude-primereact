7e6a12eea33e05b0d85bfacd4be965eedb91daa5
mxxm0x <aronr.chaves@gmail.com>
2023-06-09 20:58:06 -0300

    margin

--- src/components/QuemSomos/QuemSomos.jsx
+++ src/components/QuemSomos/QuemSomos.jsx
@@ -2,6 +2,7 @@ import './QuemSomos.css';
 import { Card } from 'primereact/card';
 import Home from '../Home/Home';
 import heroImg from '../../assets/heroImg4.jpeg';
+
 const ServicesTitle = () => {
 	return (
 		<>
@@ -51,15 +52,22 @@ const QuemSomos = () => {
 					<Card
 						border-round-right-3xl
 						title={<ServicesTitle />}
-						className="card shadow-5 hover:shadow-5 mx-auto border-round-3xl sm:border-round-3xl">
-						<div className="m-0 ">
+						className="card shadow-5 hover:shadow-5 mx-auto border-round-3xl sm:border-round-3xl"
+						style={{ height: '90%' }}>
+						<div className="m-0">
 							<h3 className="card-description text-left">
 								Nosso compromisso é fornecer serviços e produtos de Saúde,
 								Segurança e Higiene Ocupacional para melhorar as condições de
-								trabalho, promover o bem-estar e aumentar a produtividade nas
-								empresas. Trabalhamos para adequar o ambiente de trabalho às
-								normas de saúde ocupacional, garantindo um local seguro e
-								saudável para os colaboradores.
+								trabalho.
+								<br />
+								<br />
+								Nós promovemos o bem-estar e aumentamos a produtividade nas
+								empresas.
+								<br />
+								<br />
+								Trabalhamos para adequar o ambiente de trabalho às normas de
+								saúde ocupacional, garantindo um local seguro e saudável para os
+								colaboradores.
 							</h3>
 						</div>
 					</Card>
@@ -67,14 +75,17 @@ const QuemSomos = () => {
 				<div className="col-auto sm:col-12 md:col-4 lg:col-4 p-3">
 					<Card
 						title={<VisionTitle />}
-						className="card shadow-5 hover:shadow-5 mx-auto border-round-3xl sm:border-3xl">
+						className="card shadow-5 hover:shadow-5 mx-auto border-round-3xl sm:border-3xl"
+						style={{ height: '90%' }}>
 						<div className="m-0">
 							<h3 className="card-description text-left">
 								Nossa visão é ser uma empresa reconhecida pela excelência na
-								prestação de serviços, oferecendo atendimento personalizado e
-								soluções sob medida para cada cliente. Buscamos ser referência
-								no mercado, superando as expectativas e atendendo às
-								necessidades específicas de cada um.
+								prestação de serviços.
+								<br />
+								<br />
+								Oferecendo atendimento personalizado e soluções sob medida para
+								cada cliente. Buscamos ser referência no mercado, superando as
+								expectativas e atendendo às necessidades específicas de cada um.
 							</h3>
 						</div>
 					</Card>
@@ -82,12 +93,15 @@ const QuemSomos = () => {
 				<div className="col-auto sm:col-12 md:col-4 lg:col-4 p-3">
 					<Card
 						title={<ValuesTitle />}
-						className="card shadow-5 hover:shadow-5 mx-auto border-round-3xl sm:border-3xl">
+						className="card shadow-5 hover:shadow-5 mx-auto border-round-3xl sm:border-3xl"
+						style={{ height: '90%' }}>
 						<div className="m-0">
 							<h3 className="card-description text-left">
 								Nosso trabalho é guiado por valores fundamentais, como ética,
-								honestidade e integridade. Valorizamos a inovação e o dinamismo,
-								mantendo o foco no cliente em todas as nossas ações. Buscamos
+								honestidade e integridade. Valorizamos a inovação e o dinamismo.
+								<br />
+								<br />
+								Mantendo o foco no cliente em todas as nossas ações. Buscamos
 								constantemente a excelência no atendimento, oferecendo serviços
 								de qualidade e construindo relacionamentos duradouros com nossos
 								clientes.
