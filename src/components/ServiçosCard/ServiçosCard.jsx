01503263866f9b714b6e0c0e2abfcab6af5a1377
mxxm0x <aronr.chaves@gmail.com>
2023-06-09 20:21:00 -0300

    ajustando shadow

--- src/components/ServiçosCard/ServiçosCard.jsx
+++ src/components/ServiçosCard/ServiçosCard.jsx
@@ -6,7 +6,7 @@ const CardTitle01 = () => {
 	return (
 		<>
 			<div className="grid">
-				<div className="col-12 flex justify-content-center">
+				<div className="col-12 flex justify-content-center bg-black-alpha-0">
 					<MdMedicalServices className="icon" />
 				</div>
 				<div className="col-12 flex justify-content-center">
@@ -38,7 +38,7 @@ const ServiçosCard = () => {
 				<div className="col-12 sm:col-12 md:col-5 lg:col-5 p-3 lg:p-7">
 					<Card
 						title={<CardTitle01></CardTitle01>}
-						className="ServiçosCard shadow-2 hover:shadow-7">
+						className="ServiçosCard shadow-2 hover:shadow-5">
 						<div className="m-0">
 							<ul className="ServiçosCard-description">
 								<li className="ServiçosCard-items">
@@ -67,10 +67,10 @@ const ServiçosCard = () => {
 					</div> */}
 				</div>
 
-				<div className="col-12 sm:col-12 md:col-5 lg:col-5 p-3 lg:p-7 ">
+				<div className="col-12 sm:col-14 md:col-5 lg:col-5 p-3 lg:p-7 ">
 					<Card
 						title={<CardTitle02 />}
-						className="ServiçosCard border-round shadow-2 hover:shadow-7">
+						className="ServiçosCard border-round-3xl shadow-3 hover:shadow-5">
 						<div className="m-0">
 							<ul className="ServiçosCard-description">
 								<li className="ServiçosCard-items">
