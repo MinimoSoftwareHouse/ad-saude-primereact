a955d227e78e3ddbdbe3c7189a370184dcf276cc
Carlos Gabriel <carlosufpa10@gmail.com>
2023-06-09 12:16:59 -0300

    altera o número para o zap correto (amedseg)

--- src/components/Whatsapp/Whatsapp.jsx
+++ src/components/Whatsapp/Whatsapp.jsx
@@ -4,7 +4,7 @@ export default function zap(){
     return (
         <div>
             <a 
-                href="https://wa.me/5591986018137?text= Olá! Gostaria de mais informações sobre serviços/valores"
+                href="https://wa.me/5591985305630?text= Olá! Gostaria de mais informações sobre serviços/valores"
                 className='zap flex justify-content-center align-items-center' 
                 target="blank">
                 <i className='pi pi-whatsapp text-4xl'></i>
