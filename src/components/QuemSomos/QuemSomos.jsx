cb35472bce9ce72ed64a07db9cb0026690570401
mxxm0x <aronr.chaves@gmail.com>
2023-06-09 18:40:23 -0300

    adicionando heroImg e componente Home

--- src/components/QuemSomos/QuemSomos.jsx
+++ src/components/QuemSomos/QuemSomos.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import './QuemSomos.css';
-import {Card} from 'primereact/card';
-
-
+import { Card } from 'primereact/card';
+import Home from '../Home/Home';
+import heroImg from '../../assets/heroImg4.jpeg';
 const ServicesTitle = () => {
 	return (
 		<>
@@ -13,7 +12,7 @@ const ServicesTitle = () => {
 			</div>
 		</>
 	);
-}
+};
 
 const VisionTitle = () => {
 	return (
@@ -25,7 +24,7 @@ const VisionTitle = () => {
 			</div>
 		</>
 	);
-}
+};
 
 const ValuesTitle = () => {
 	return (
@@ -37,58 +36,66 @@ const ValuesTitle = () => {
 			</div>
 		</>
 	);
-}
-
-
+};
 
 const QuemSomos = () => {
 	return (
+		<>
+			<Home
+				title="Quem Somos"
+				heroImg={heroImg}
+			/>
 
-		<div className="grid justify-content-around">
-			<div className="col-12 sm:col-12 md:col-4 lg:col-4 p-4">
-
-				<Card title={<ServicesTitle/>} className="card shadow-2 hover:shadow-7 mx-0">
-					<div className="m-0">
-						<h3 className="card-description">
-							Oferecer serviços e produtos de Saúde, Segurança e Higiene
-							Ocupacional, que promovam melhores condições de trabalho, bem estar
-							social, qualidade de vida e aumento da produtividade nas empresas,
-							através da adequação do ambiente de trabalho às normas de saúde
-							ocupacional.
-						</h3>
-					</div>
-				</Card>
-
+			<div className="grid justify-content-around">
+				<div className="col-12 sm:col-12 md:col-4 lg:col-4 p-4">
+					<Card
+						title={<ServicesTitle />}
+						className="card shadow-2 hover:shadow-7 mx-0">
+						<div className="m-0 ">
+							<h3 className="card-description">
+								Nosso compromisso é fornecer serviços e produtos de Saúde,
+								Segurança e Higiene Ocupacional para melhorar as condições de
+								trabalho, promover o bem-estar e aumentar a produtividade nas
+								empresas. Trabalhamos para adequar o ambiente de trabalho às
+								normas de saúde ocupacional, garantindo um local seguro e
+								saudável para os colaboradores.
+							</h3>
+						</div>
+					</Card>
+				</div>
+				<div className="col-12 sm:col-12 md:col-4 lg:col-4 p-4">
+					<Card
+						title={<VisionTitle />}
+						className="card shadow-2 hover:shadow-7 mx-0">
+						<div className="m-0">
+							<h3 className="card-description">
+								Nossa visão é ser uma empresa reconhecida pela excelência na
+								prestação de serviços, oferecendo atendimento personalizado e
+								soluções sob medida para cada cliente. Buscamos ser referência
+								no mercado, superando as expectativas e atendendo às
+								necessidades específicas de cada um.
+							</h3>
+						</div>
+					</Card>
+				</div>
+				<div className="col-12 sm:col-12 md:col-4 lg:col-4 p-4">
+					<Card
+						title={<ValuesTitle />}
+						className="card shadow-2 hover:shadow-7 mx-0">
+						<div className="m-0">
+							<h3 className="card-description">
+								Nosso trabalho é guiado por valores fundamentais, como ética,
+								honestidade e integridade. Valorizamos a inovação e o dinamismo,
+								mantendo o foco no cliente em todas as nossas ações. Buscamos
+								constantemente a excelência no atendimento, oferecendo serviços
+								de qualidade e construindo relacionamentos duradouros com nossos
+								clientes.
+							</h3>
+						</div>
+					</Card>
+				</div>
 			</div>
-			<div className="col-12 sm:col-12 md:col-4 lg:col-4 p-4">
-
-
-				<Card title={<VisionTitle/>} className="card shadow-2 hover:shadow-7 mx-0">
-					<div className="m-0">
-						<h3 className="card-description">
-							Trabalhamos para ser uma empresa diferenciada na prestação de
-							serviços de forma a garantir a nossos clientes, serviços de
-							qualidade com atendimento personalizado visando à necessidade de
-							cada um.
-						</h3>
-					</div>
-				</Card>
-
-
-			</div>
-			<div className="col-12 sm:col-12 md:col-4 lg:col-4 p-4">
-
-				<Card title={<ValuesTitle/>} className="card shadow-2 hover:shadow-7 mx-0">
-					<div className="m-0">
-						<h3 className="card-description">
-							Trabalho, Ética, Honestidade, Integridade, Inovação, Dinamismo, Foco
-							no cliente e Excelência no atendimento.
-						</h3>
-					</div>
-				</Card>
-
-			</div>
-		</div>
+		</>
 	);
 };
 
